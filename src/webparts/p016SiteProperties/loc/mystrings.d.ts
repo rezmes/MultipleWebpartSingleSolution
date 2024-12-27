5d003d947774937453fa3c1fec80b9b636768cd4
rezmes <rezmes@gmail.com>
2024-12-27 14:38:32 +0330

    P016SiteProperties

---
+++ src/webparts/p016SiteProperties/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IP016SitePropertiesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'P016SitePropertiesWebPartStrings' {
+  const strings: IP016SitePropertiesWebPartStrings;
+  export = strings;
+}
